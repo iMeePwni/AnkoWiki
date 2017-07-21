fd004c6a1b4e13f81a041615a5f9ead5568f95f2
guofeng <imeepwni@outlook.com>
2017-07-21 08:39:28 +0800

    改为简单的dataBinding

--- app/src/main/java/com/imeepwni/android/ankowiki/ui/MainFragment.kt
+++ app/src/main/java/com/imeepwni/android/ankowiki/ui/MainFragment.kt
@@ -1,97 +1,21 @@
 package com.imeepwni.android.ankowiki.ui
 
-import android.content.*
 import android.databinding.*
 import android.os.*
 import android.support.v4.app.*
 import android.support.v7.widget.*
-import android.text.*
 import android.view.*
 import com.imeepwni.android.ankowiki.*
 import com.imeepwni.android.ankowiki.databinding.*
 import com.imeepwni.android.ankowiki.model.*
+import com.imeepwni.android.ankowiki.vm.*
 import org.jetbrains.anko.*
-import org.jetbrains.anko.appcompat.v7.*
-import org.jetbrains.anko.support.v4.*
 
 class MainFragment : Fragment(),
         AnkoLogger {
 
     val ankoList by lazy {
-        arrayListOf<Anko>().apply {
-            add(Anko("toast") {
-                toast("Hi there!")
-            })
-            add(Anko("longToast") {
-                longToast("Wow, such a duration")
-            })
-            add(Anko("alerts") {
-                alert("Hi, I'm Roy", "Have you tried turning it off and on again?") {
-                    yesButton { toast("Oh…") }
-                    noButton { toast("you clicked no!") }
-                }.show()
-            })
-            add(Anko("appCompat") {
-                alert {
-                    customView {
-                        val editText = tintedEditText()
-                        yesButton { toast("${editText.text}") }
-                    }
-                }.show()
-            })
-            add(Anko("selectors") {
-                val list = arrayListOf("Russia", "USA", "Japan", "Australia")
-                selector("Where are you from?", list) {
-                    _, i ->
-                    toast("So you're living in ${list[i]}")
-                }
-            })
-            add(Anko("Progress dialogs") {
-                progressDialog("Please wait a bit...", "Fetching data") {
-                    setButton(DialogInterface.BUTTON_POSITIVE, "ok") {
-                        _, _ ->
-                        toast(" ok")
-                    }
-                    setButton(DialogInterface.BUTTON_NEGATIVE, "cancel") {
-                        _, _ ->
-                        cancel()
-                    }
-                }
-            })
-            add(Anko("Make a call") {
-                alert {
-                    customView {
-                        val phone = tintedEditText {
-                            inputType = InputType.TYPE_CLASS_PHONE
-                        }
-                        yesButton {
-                            makeCall(phone.text.toString())
-                        }
-                    }
-                }.show()
-            })
-            add(Anko("sendSMS") {
-                alert {
-                    customView {
-                        val text = tintedEditText {
-                            inputType = InputType.TYPE_CLASS_TEXT
-                        }
-                        yesButton {
-                            activity.sendSMS("18566235740", text.text.toString())
-                        }
-                    }
-                }.show()
-            })
-            add(Anko("Browse The web"){
-                browse("https://www.baidu.com")
-            })
-            add(Anko("share some text"){
-                share("text", "subject")
-            })
-            add(Anko("SendAnEmail"){
-                email("email", "subject", "haha")
-            })
-        }
+        AnkoLab(activity).getAnkoList()
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
@@ -121,10 +45,8 @@ class MainFragment : Fragment(),
 
     inner class ViewHolder(val binding: LayoutListItemBinding) : RecyclerView.ViewHolder(binding.button) {
         fun bind(anko: Anko) {
-            (binding.button).run {
-                text = anko.name
-                setOnClickListener { anko.function() }
-            }
+            binding.viewModel = AnkoViewModel(anko)
+            binding.executePendingBindings()
         }
     }
 
---
+++ app/src/main/java/com/imeepwni/android/ankowiki/vm/AnkoViewModel.kt
@@ -0,0 +1,16 @@
+package com.imeepwni.android.ankowiki.vm
+
+import android.databinding.*
+import com.imeepwni.android.ankowiki.model.*
+
+/**
+ * Create by guofeng on 2017/7/20.
+ */
+class AnkoViewModel(var anko: Anko) : BaseObservable() {
+
+    fun getAnkoName() = anko.name
+
+    fun doAnkoFunction() {
+        anko.function()
+    }
+}
